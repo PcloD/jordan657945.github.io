64c0d4ac4480fae0512affbb1f136bb7c136c1f4
Jordan657945 <jordanhasty@yahoo.com>
2016-08-28 20:43:45 -0400

    update

---
+++ audiovisualizer/script.js
@@ -0,0 +1,262 @@
+// establish vars
+var canvas, ctx, source, context, analyser, fbc_array, rads,
+center_x, center_y, radius, radius_old, deltarad, shockwave,
+bars, bar_x, bar_y, bar_x_term, bar_y_term, bar_width,
+bar_height, react_x, react_y, intensity, grd, rot, inputURL,
+JSONPThing, JSONResponse, soundCloudTrackName, audio, pause,
+artist, title, img_url;
+
+var client_id = "8df0d68fcc1920c92fc389b89e7ce20f";
+
+// give vars an initial real value to validate
+bars = 200;
+react_x = 0;
+react_y = 0;
+radius = 0;
+deltarad = 0;
+shockwave = 0;
+rot = 0;
+intensity = 0;
+pause = 1;
+
+function initPage() {
+	canvas = document.getElementById("visualizer_render");
+	ctx = canvas.getContext("2d");
+	
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+
+	resize_canvas();
+	document.getElementById("artwork").style.opacity = 0;
+}
+
+function resize_canvas() {
+	if (canvas.width  != window.innerWidth) {
+		canvas.width  = window.innerWidth;
+	}
+
+	if (canvas.height != window.innerHeight) {
+		canvas.height = window.innerHeight;
+	}
+	
+	var bg = ctx.createLinearGradient(0, 0, 0, canvas.height);
+	bg.addColorStop(0, "rgb(180, 140, 230)");
+	bg.addColorStop(1, "rgb(102, 102, 255)");
+	ctx.fillStyle = bg;
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	
+	ctx.fillStyle = "rgb(255, 255, 255)";
+	ctx.beginPath();
+	ctx.arc(canvas.width * 0.5, canvas.height * 0.5, 25, 0, Math.PI * 2, false);
+	ctx.fill();
+}
+
+(function() {
+    var mouseTimer = null, cursorVisible = true;
+
+    function disappearCursor() {
+        mouseTimer = null;
+        document.body.style.cursor = "none";
+		document.getElementById("hideHead").style.opacity = 0;
+		document.getElementById("hideBody").style.opacity = 0;
+        cursorVisible = false;
+    }
+
+    document.onmousemove = function() {
+        if (mouseTimer) {
+            window.clearTimeout(mouseTimer);
+        }
+        if (!cursorVisible) {
+            document.body.style.cursor = "default";
+			document.getElementById("hideHead").style.opacity = 100;
+			document.getElementById("hideBody").style.opacity = 100;
+            cursorVisible = true;
+        }
+        mouseTimer = window.setTimeout(disappearCursor, 5000);
+    };
+})();
+
+function getJSON(url, callback) {
+	JSONPThing = document.createElement("script");
+	JSONPThing.type = "text/javascript";
+	JSONPThing.src = url + "&callback=" + callback.name;
+	document.body.appendChild(JSONPThing);
+}
+
+function userJSONCallback(data) {
+	document.body.removeChild(JSONPThing); // required
+	JSONPThing = null;
+	
+	var user_id = data.id;
+	artist = data.username;
+	
+	var tracks = "https://api.soundcloud.com/users/" + user_id + "/tracks.json?client_id=" + client_id + "&limit=200";
+
+	getJSON(tracks, tracksJSONCallback); // continues in tracksJSONCallback
+}
+
+function tracksJSONCallback(data) {
+	document.body.removeChild(JSONPThing); // required
+	JSONPThing = null;
+	
+	// go through each object (track) in array (data)
+	for(var i = 0; i < data.length; i++) {
+		var track = data[i];
+		// check each track with the name (input URL)
+		if(track.permalink == soundCloudTrackName) {
+			inputURL = track.stream_url + "?client_id=" + client_id;
+			title = track.title;
+			img_url = track.artwork_url;
+			
+			initMp3Player();
+			break;
+		}
+	}
+}
+
+function handleButton()
+{
+	var inputURL = document.getElementById("input_URL").value;
+	if(inputURL.search("soundcloud.com") != -1 && inputURL.search("api.soundcloud.com") == -1) { // is it a regular old soundcloud link
+		var splitBySlash = inputURL.replace(/http:\/\/|https:\/\//gi, "").split("/"); // get rid of "http://" / "https://" in front of url and then split by slashes
+		if(splitBySlash.length == 3) { // make sure there's an actual song included at the end
+			var soundCloudUserURL = "http://" + splitBySlash[0] + "/" + splitBySlash[1];
+			soundCloudTrackName = splitBySlash[2];
+			var apiResovleURL = "https://api.soundcloud.com/resolve.json?url=" + soundCloudUserURL + "&client_id=" + client_id;
+			getJSON(apiResovleURL, userJSONCallback); // continues in userJSONCallback
+		}
+		else if (splitBySlash.legnth == 2) { // there's a user but no song
+			// do whatever here
+		}
+		else {
+			// do whatever here
+		}
+	}
+}
+
+function togglepause() {
+	if(pause) {
+			pause = 0;
+			audio.play();
+		} else {
+			pause = 1;
+			audio.pause();
+		}
+}
+
+function autoSelect() {
+	document.getElementById("input_URL").select();
+	
+}
+			
+function initMp3Player() {
+	// audio object
+	if(audio != undefined) {
+		audio.pause();
+		audio = undefined;
+	}
+	
+	audio = new Audio();
+	audio.src = inputURL;
+	audio.crossOrigin = "anonymous";
+	audio.controls = false;
+	audio.loop = false;
+	audio.autoplay = false;
+	
+	context = new AudioContext();
+	analyser = context.createAnalyser();
+	// route audio playback
+	source = context.createMediaElementSource(audio);
+	source.connect(analyser);
+	analyser.connect(context.destination);
+	frameLooper();
+	
+	pause = 0;
+	audio.play();
+	
+	document.getElementById("artistname").innerHTML = artist;
+	document.getElementById("songname").innerHTML = title;
+	document.getElementById("pagetitle").innerHTML = title;
+	document.getElementById("artwork").src = img_url;
+	
+	document.getElementById("artwork").style.opacity = 100;
+	document.getElementById("artwork").style.border = "5px solid #f174eb";
+	document.getElementById("artwork").style.padding= "1px";
+}
+			
+function frameLooper() {
+	window.requestAnimationFrame(frameLooper);
+				
+	grd = ctx.createLinearGradient(0, 0, 0, canvas.height);
+	grd.addColorStop(0, "rgba(180, 140, 230, 0.5)");
+	grd.addColorStop(1, "rgba(102, 102, 255, 0.5)");
+
+	ctx.fillStyle = grd;
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+		
+	rot = rot + intensity * 0.0000001;
+		
+	react_x = 0;
+	react_y = 0;
+				
+	intensity = 0;
+				
+	fbc_array = new Uint8Array(analyser.frequencyBinCount);
+	analyser.getByteFrequencyData(fbc_array);
+	
+	for (var i = 0; i < bars; i++) {
+		rads = Math.PI * 2 / bars;
+						
+		bar_x = center_x + Math.cos(rads * i + rot) * radius;
+		bar_y = center_y + Math.sin(rads * i + rot) * radius;
+				
+		bar_width = 4;
+		bar_height = Math.min(99999, Math.max((fbc_array[i] * 2.5 - 200), 0));
+						
+		bar_x_term = center_x + Math.cos(rads * i + rot) * (radius + bar_height);
+		bar_y_term = center_y + Math.sin(rads * i + rot) * (radius + bar_height);
+						
+		ctx.strokeStyle = "rgb(" + (fbc_array[i]).toString() + ", " + 255 + ", " + 255 + ")";
+		ctx.lineWidth = bar_width;
+		ctx.beginPath();
+		ctx.moveTo(bar_x, bar_y);
+		ctx.lineTo(bar_x_term, bar_y_term);
+		ctx.stroke();
+					
+		react_x += Math.cos(rads * i + rot) * (radius + bar_height);
+		react_y += Math.sin(rads * i + rot) * (radius + bar_height);
+					
+		intensity += bar_height;
+	}
+				
+	center_x = canvas.width / 2 - (react_x * 0.01);
+	center_y = canvas.height / 2 - (react_y * 0.01);
+				
+	radius_old = radius;
+	radius =  15 + (intensity * 0.002);
+	deltarad = radius - radius_old;
+				
+	ctx.fillStyle = "rgb(255, 255, 255)";
+	ctx.beginPath();
+	ctx.arc(center_x, center_y, radius + 10, 0, Math.PI * 2, false);
+	ctx.fill();
+	
+	// shockwave effect			
+	shockwave += 60;
+				
+	ctx.lineWidth = 15;
+	ctx.strokeStyle = "rgb(255, 255, 255)";
+	ctx.beginPath();
+	ctx.arc(center_x, center_y, shockwave + radius, 0, Math.PI * 2, false);
+	ctx.stroke();
+				
+				
+	if (deltarad > 15) {
+		shockwave = 0;
+		
+		ctx.fillStyle = "rgba(255, 255, 255, 0.5)";
+		ctx.fillRect(0, 0, canvas.width, canvas.height);
+		
+		rot = rot + Math.random() * 10//rot + 0.2
+	}
+}
